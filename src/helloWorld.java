8ddc40f0d6463641f8c336060ddaa37a655f8ef4
SpikeYr <emil.f.anderson@gmail.com>
2018-09-05 16:24:36 +0200

    Quadrant

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,4 +1,54 @@
+import java.util.Scanner;
 
 public class helloWorld {
 
-}
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Input two integers");
+
+		int x = input.nextInt();
+		int y = input.nextInt();
+
+		input.close(); //END Scanner
+
+		if (x > 0 && y > 0) {
+			System.out.println(1);
+		}
+
+		if (x > 0 && y < 0) {
+			System.out.println(4);
+		}
+
+		if (x < 0 && y < 0) {
+			System.out.println(2);
+		}
+
+		if (x < 0 && y > 0) {
+			System.out.println(3);
+		}
+
+		if (x > 0 && y == 0) {
+			System.out.println(41);
+		}
+
+		if (x == 0 && y < 0) {
+			System.out.println(12);
+		}
+
+		if (x < 0 && y == 0) {
+			System.out.println(23);
+		}
+
+		if (x == 0 && y > 0) {
+			System.out.println(34);
+		}
+
+		if (x == 0 && y == 0) {
+			System.out.println("Origin");
+		}
+
+	} // END main
+
+} // END helloWorld
