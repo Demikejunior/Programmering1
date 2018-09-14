61436245965ffd09a6c98024a7471cd3f091676e
SpikeYr <emil.f.anderson@gmail.com>
2018-09-14 14:25:44 +0200

    Villkor commit 2
    Importerar Scanner, samt skapar en Scanner som heter input, som sedan stängs.

--- Läxa v.36/Villkor.java
+++ Läxa v.36/Villkor.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 
 public class Villkor {
 	
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		
+		input.close();
 		
 	}
 	
