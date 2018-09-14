bfd46f97b747aadf3d935becd5511d438ec16613
SpikeYr <emil.f.anderson@gmail.com>
2018-09-14 14:55:38 +0200

    Villkor commit 3

--- Villkor/Villkor.java
+++ Villkor/Villkor.java
@@ -6,10 +6,59 @@ public class Villkor {
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Skriv in 10 heltal: ");
 		
+		int tal0 = input.nextInt();
+		
+		int tal1 = input.nextInt();
+		
+		int tal2 = input.nextInt();
+		
+		int tal3 = input.nextInt();
+		
+		int tal4 = input.nextInt();
+		
+		int tal5 = input.nextInt();
+		
+		int tal6 = input.nextInt();
+		
+		int tal7 = input.nextInt();
+		
+		int tal8 = input.nextInt();
+		
+		int tal9 = input.nextInt();
 		
 		
 		input.close();
+		
+		
+		if(tal0 > tal1) {
+			if(tal0 > tal2) {
+				if(tal0 > tal3) {
+					if(tal0 > tal4) {
+						if(tal0 > tal5) {
+							if(tal0 > tal6) {
+								if(tal0 > tal7) {
+									if(tal0 > tal8) {
+										if(tal0 > tal9) {
+											
+										}
+										
+										else{
+											
+										}
+										
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		
+		
 	}
 	
 }
