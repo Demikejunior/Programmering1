a2af2f343860723da22e6215476c12f44db0b20b
SpikeYr <emil.f.anderson@gmail.com>
2018-09-19 16:40:15 +0200

    Loops inlämning 5 - Now with commentary!

--- src/v38_Loops.java
+++ src/v38_Loops.java
@@ -3,23 +3,36 @@ import java.util.Scanner;
 public class v38_Loops {
 
 	public static void main(String[] args) {
+		
+		
+		//Inlämning 1
+		
 		double sum = 0;
 		for (int i = 0; i < 100; i++) {
 			sum = sum + i;
 		}
 		System.out.println(sum);
+		
+		
+		//Inlämning 2
 
 		for (int i = 25; i <= 50; i++) {
 
 			System.out.println(i);
 
 		}
+		
+		
+		//Inlämning 3
 
 		for (int i = 20; i >= 0; i--) {
 			
 			System.out.println(i);
 			
 		}
+		
+		
+		//Inlämning 4
 
 		Scanner input = new Scanner(System.in);
 		
@@ -32,6 +45,29 @@ public class v38_Loops {
 			System.out.print(i*var + " ");
 			
 		}
+		
+		
+		//Inlämning 5
+		
+		boolean exit = false;
+		
+		System.out.println("\n\rSkriv in nummer");
+		
+		sum = 0;
+		
+		while (!exit) {
+			int tal1 = input.nextInt();
+			
+			if (tal1 == 0) {
+				exit = true;
+			}
+			
+			sum = tal1 + sum;
+			
+		}
+		System.out.println(sum);
+		
+		
 
 		input.close();
 
