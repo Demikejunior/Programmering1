8fae07e73dcdb6f3ee675c60d9b0da6e893987d0
SpikeYr <emil.f.anderson@gmail.com>
2018-09-19 15:55:24 +0200

    Loops inlämning 1

---
+++ src/v38_Loops.java
@@ -0,0 +1,14 @@
+
+public class v38_Loops {
+
+	public static void main(String[] args) {
+		
+		for (int i = Integer.MIN_VALUE; i < 100; i++) {
+			
+			System.out.println(i);
+			
+		}
+		
+	}
+	
+}
