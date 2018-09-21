06cbf971ce1b5cd9318618db771cfb0846061643
SpikeYr <emil.f.anderson@gmail.com>
2018-09-21 15:49:15 +0200

    Kattis Trik start

---
+++ src/kattis/Trik.java
@@ -0,0 +1,30 @@
+package kattis;
+
+import java.util.Scanner;
+
+public class Trik {
+
+	public static void main(String[] args) {
+		
+		int ballPos = 0;
+		
+		boolean exit = false;
+		
+		Scanner input = new Scanner(System.in);
+		
+		String move = input.nextLine();
+		
+		move.split("(?!^)");
+		
+		while (!exit) {
+			
+			
+			
+			
+		}
+		
+		input.close();
+		
+	}
+	
+}
