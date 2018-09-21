268750ab4f42ddbec6e8d14c101d07a25f3ab6dc
SpikeYr <emil.f.anderson@gmail.com>
2018-09-21 14:10:36 +0200

    Kattis commit 1
    helloWorld(quadrant selection) flyttad till kattis folder. tigerWoods är Hello World! uppgiften. twoStones är Two Stones uppgiften

---
+++ Kattis/twoStones.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class twoStones {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int var = input.nextInt();
+		if (var % 2 == 0) {
+			System.out.println("Bob");
+		} else {
+			System.out.println("Alice");
+		}
+	}
+	
+}
