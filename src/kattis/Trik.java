c321b94205d50aa02e813b6cacdf2271139815e0
SpikeYr <emil.f.anderson@gmail.com>
2018-09-25 16:14:51 +0200

    Kattis Trik commit 2

--- src/kattis/Trik.java
+++ src/kattis/Trik.java
@@ -1,5 +1,6 @@
 package kattis;
 
+import java.sql.Array;
 import java.util.Scanner;
 
 public class Trik {
@@ -7,21 +8,46 @@ public class Trik {
 	public static void main(String[] args) {
 		
 		int ballPos = 0;
+
+		char move[] = new char[50];
 		
-		boolean exit = false;
 		
 		Scanner input = new Scanner(System.in);
 		
-		String move = input.nextLine();
+		String str = input.nextLine().toUpperCase();
 		
-		move.split("(?!^)");
+		move = str.toCharArray();
 		
-		while (!exit) {
-			
-			
+		
+		for (int i = 0; i < move.length ; i++) {
+			if (move[i] == 'A'){
+				if(ballPos == 0) {
+					ballPos = 1;
+				}
+				if(ballPos == 1) {
+					ballPos = 0;
+				}
+			}
+			if (move[i] == 'B'){
+				if(ballPos == 1) {
+					ballPos = 2;
+				}
+				if(ballPos == 2) {
+					ballPos = 1;
+				}
+			}
+			if (move[i] == 'C'){
+				if(ballPos == 0) {
+					ballPos = 2;
+				}
+				if(ballPos == 2) {
+					ballPos = 0;
+				}
+			}
 			
 			
 		}
+		System.out.println(ballPos);
 		
 		input.close();
 		
