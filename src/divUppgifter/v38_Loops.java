70eaf5bee2233a86843f763e85daf4f20f080437
SpikeYr <emil.f.anderson@gmail.com>
2018-09-26 15:39:40 +0200

    Loop div förbättringar

--- src/divUppgifter/v38_Loops.java
+++ src/divUppgifter/v38_Loops.java
@@ -8,6 +8,7 @@ public class v38_Loops {
 		Scanner input = new Scanner(System.in);
 		
 		
+		
 		//Inlämning 1
 		
 		double sum = 0;
@@ -15,7 +16,8 @@ public class v38_Loops {
 			sum = sum + i;
 		}
 		System.out.println(sum);
-		System.out.println("\rFärdig med Inlämning 1");
+		System.out.println("\rFärdig med Inlämning 1\r");
+		
 		
 		
 		//Inlämning 2
@@ -25,7 +27,8 @@ public class v38_Loops {
 			System.out.println(i);
 
 		}
-		System.out.println("\rFärdig med Inlämning 2");
+		System.out.println("\rFärdig med Inlämning 2\r");
+		
 		
 		
 		//Inlämning 3
@@ -35,12 +38,12 @@ public class v38_Loops {
 			System.out.println(i);
 			
 		}
-		System.out.println("\rFärdig med Inlämning 3");
+		System.out.println("\rFärdig med Inlämning 3\r");
+		
 		
 		
 		//Inlämning 4
 
-		
 		System.out.println("Skriv ett heltal mellan 0 och 10");
 		
 		byte var = input.nextByte();
@@ -50,7 +53,8 @@ public class v38_Loops {
 			System.out.print(i*var + " ");
 			
 		}
-		System.out.println("\rFärdig med Inlämning 4");
+		System.out.println("\rFärdig med Inlämning 4\r");
+		
 		
 		
 		//Inlämning 5
@@ -72,14 +76,15 @@ public class v38_Loops {
 			
 		}
 		System.out.println(sum);
-		System.out.println("\rFärdig med Inlämning 5");
+		System.out.println("\rFärdig med Inlämning 5\r");
+		
 		
 		
 		//Inlämning 6
 		
 		exit = false;
 		
-		System.out.println("\rSkriv in ord");
+		System.out.println("\rSkriv in ord\r");
 		
 		
 		while (!exit) {
@@ -90,12 +95,17 @@ public class v38_Loops {
 			}
 			
 		}
-		System.out.println("\rFärdig med Inlämning 6");
+		System.out.println("\rFärdig med Inlämning 6\r");
+		
 		
 		
 		//Inlämning 7
 		
-		for (int i = 0; i < 4; i++) {
+		System.out.println("Skriv in ett heltal");
+		
+		int ecks = input.nextInt();
+		
+		for (int i = 0; i < ecks; i++) {
 			
 			for (int j = 0 ; j <= i ; j++) {
 				
@@ -105,15 +115,19 @@ public class v38_Loops {
 			System.out.println("");
 			
 		}
-		System.out.println("\rFärdig med Inlämning 7");
+		System.out.println("\rFärdig med Inlämning 7\r");
+		
 		
 
 		//Inlämning 8
 		
-		for (int i = 100; i <= 1000; i += 2) {
-			System.out.println(i);
+		for (int i = 100; i <= 1000; i ++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
 		}
-		System.out.println("\rFärdig med Inlämning 8");
+		System.out.println("\rFärdig med Inlämning 8\r");
+		
 		
 		
 		//Inläming 9
@@ -127,7 +141,8 @@ public class v38_Loops {
 			System.out.print(fac + " * " + i + " => ");
 		}
 		System.out.println("\r" + fac);
-		System.out.println("\rFärdig med Inlämning 9");
+		System.out.println("\rFärdig med Inlämning 9\r");
+		
 		
 		
 		//Inlämning fibonacci
@@ -144,6 +159,7 @@ public class v38_Loops {
 			fib3 = fib1+ fib2;
 		}
 		System.out.print(fib1 + " " + fib2 + " " + fib3 + " ");
+		System.out.println("\rFärdig med Fibonacci\r");
 
 		input.close();
 
