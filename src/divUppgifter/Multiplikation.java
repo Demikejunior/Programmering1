bfd58532fd9053879e4f1a81c9d98ad8d1f06d4b
SpikeYr <emil.f.anderson@gmail.com>
2018-09-28 16:43:23 +0200

    Klassuppgift commit 4 - Multiplikation

---
+++ src/divUppgifter/Multiplikation.java
@@ -0,0 +1,35 @@
+package divUppgifter;
+
+import java.util.Scanner;
+
+public class Multiplikation {
+	public static void main(String[] args) {
+
+		boolean exit = false;
+		
+		Scanner input = new Scanner(System.in);
+		
+		int var = 1;
+		int count = 0;
+		
+		while (!exit) {
+			
+			if (var >= 10000 || count >= 10) {
+				exit = true;
+			} else {
+			
+			int var1 = input.nextInt();
+			
+			var = var * var1;
+			
+			count++;
+			}
+		}
+		
+		input.close();
+		
+		System.out.println(var);
+		
+	}
+
+}
