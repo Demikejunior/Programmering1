7cd16e02dfd65c05325c1b159d2ca55a7fd16667
SpikeYr <emil.f.anderson@gmail.com>
2018-10-01 10:44:59 +0200

    Klassuppgift commit 6 - Cylinder

---
+++ src/divUppgifter/Cylinder.java
@@ -0,0 +1,21 @@
+package divUppgifter;
+
+import java.util.Scanner;
+
+public class Cylinder {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Skriv en radie, och en höjd");
+		
+		Scanner input = new Scanner(System.in);
+		
+		double area = input.nextDouble() * 3.14;
+		
+		double height = input.nextDouble();
+		
+		System.out.println("Cylinderns area är: " + area * height);
+		
+	}
+	
+}
