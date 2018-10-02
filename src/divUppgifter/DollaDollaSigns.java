370c9520a3bd5bffd2a4b1559fd4bfb62cc5cd9c
SpikeYr <emil.f.anderson@gmail.com>
2018-10-02 10:14:59 +0200

    Klassuppgift commit 7 - DollaDollaSquare

---
+++ src/divUppgifter/DollaDollaSigns.java
@@ -0,0 +1,35 @@
+package divUppgifter;
+
+import java.util.Scanner;
+
+public class DollaDollaSigns {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		int dolla = input.nextInt();
+
+		input.close();
+		for (int i = 0; i < dolla; i++) {
+			System.out.print("$");
+		}
+		System.out.println();
+		for (int i = 0; i < dolla - 2; i++) {
+
+			System.out.print("$");
+			for (int j = 0; j < dolla - 2; j++) {
+				System.out.print(" ");
+			}
+			System.out.print("$");
+			System.out.println();
+
+		}
+
+		for (int i = 0; i < dolla; i++) {
+			System.out.print("$");
+		}
+
+	}
+
+}
