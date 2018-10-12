f824efaa805417f3774e9141cdab00df6848d113
SpikeYr <emil.f.anderson@gmail.com>
2018-10-12 15:51:01 +0200

    Maetoder 2 commit 3 - Rövarspråk början

--- src/divUppgifter/Methods_v2.java
+++ src/divUppgifter/Methods_v2.java
@@ -2,24 +2,24 @@ package divUppgifter;
 
 public class Methods_v2 {
 	public static void main(String[] args) {
-		System.out.println(count("abacabadabacaba", 'd'));
+
 	}
-	
+
 	public static double volume(double radius) {
 		double r = (Math.pow(radius, 3) * Math.PI) / 4;
 		return r;
 	}
-	
+
 	public static String reverse(String str) {
 		int l = str.length();
 		char[] arr = new char[l];
 		for (int i = l - 1; i >= 0; i--) {
-			arr[l-(i+1)] = str.charAt(i);
+			arr[l - (i + 1)] = str.charAt(i);
 		}
 		String s = new String(arr);
-		return s;	
+		return s;
 	}
-	
+
 	public static int count(String str, char c) {
 		int l = str.length();
 		int t = 0;
@@ -30,5 +30,20 @@ public class Methods_v2 {
 		}
 		return t;
 	}
-	
+
+	public static String sjorovare(String str) {
+		int l = str.length();
+		String s = "";
+		int a = 0;
+		for (int i = 0; i < l; i++) {
+			if (str.toUpperCase().charAt(i) == ('B' | 'C' | 'D' | 'F' | 'G' | 'H' | 'J' | 'K' | 'L' | 'M' | 'N' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'V' | 'Z')) {
+				if ()
+			}
+		
+		}
+		
+		return s;
+		
+	}
+
 }
