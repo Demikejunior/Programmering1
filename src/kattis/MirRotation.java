4e059a37d67dfd8761356e3ba30a8d2bb6edf11e
Demikejunior <emil.f.anderson@gmail>
2018-12-12 16:23:28 +0100

    Kattis Rot - start

---
+++ src/kattis/MirRotation.java
@@ -0,0 +1,23 @@
+package kattis;
+
+import java.util.Scanner;
+
+public class MirRotation {
+	
+	public static void main(String[] args) {
+		char[] alpha = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', '.'};
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int var = sc.nextInt();
+			
+			if (var == 0) {
+				break;
+			}
+			
+		}
+		sc.close();
+		
+	}
+	
+}
