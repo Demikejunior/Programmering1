8e4245572b26e8a96858f44c81d5b17c9dbb33c0
SpikeYr <emil.f.anderson@gmail.com>
2019-01-14 14:16:25 +0100

    I dunno

--- src/kattis/Tree.java
+++ src/kattis/Tree.java
@@ -9,18 +9,14 @@ public class Tree {
 		
 		int height = sc.nextInt();
 		char[] path = sc.nextLine().toUpperCase().toCharArray();
-		double tot = Math.pow(2, height + 1) - 1;
+		
+		
 		
 		for (int i = 0; i < path.length; i++) {
-			 if (path[i] == 'R') {
-				tot = tot - (Math.pow(2, i));
-			} else if (path[i] == 'L') {
-				tot = tot - (Math.pow(2, i) - 1);
-			}
-			 System.out.println(tot);
+			
 		}
 		
-		int sol = (int) tot; 
+		int sol = 0; 
 		System.out.println(sol);
 		
 		sc.close();
