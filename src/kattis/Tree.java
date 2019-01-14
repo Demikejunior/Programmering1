244aaa790c3c7a8dc752c0e241cc5039d548174d
SpikeYr <emil.f.anderson@gmail.com>
2019-01-14 14:16:44 +0100

    Revert "I dunno"
    This reverts commit 8e4245572b26e8a96858f44c81d5b17c9dbb33c0.

--- src/kattis/Tree.java
+++ src/kattis/Tree.java
@@ -9,14 +9,18 @@ public class Tree {
 		
 		int height = sc.nextInt();
 		char[] path = sc.nextLine().toUpperCase().toCharArray();
-		
-		
+		double tot = Math.pow(2, height + 1) - 1;
 		
 		for (int i = 0; i < path.length; i++) {
-			
+			 if (path[i] == 'R') {
+				tot = tot - (Math.pow(2, i));
+			} else if (path[i] == 'L') {
+				tot = tot - (Math.pow(2, i) - 1);
+			}
+			 System.out.println(tot);
 		}
 		
-		int sol = 0; 
+		int sol = (int) tot; 
 		System.out.println(sol);
 		
 		sc.close();
