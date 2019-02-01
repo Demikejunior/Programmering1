74de20c206215c5c5911db40f755d8865750660d
SpikeYr <emil.f.anderson@gmail.com>
2019-02-01 18:31:44 +0100

    Kattis NeedForSpeed - commit 1

---
+++ src/kattis/NeedForSpeed.java
@@ -0,0 +1,26 @@
+package kattis;
+
+import java.util.Scanner;
+
+public class NeedForSpeed {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double n = sc.nextInt();
+		double t = sc.nextInt();
+		double d= 0;
+		
+		for (int i = 0; i < n; i++) {
+			d += sc.nextInt() ;
+		}
+		double tr = (d / t);
+		System.out.println(tr);
+		
+		
+		sc.close();
+
+	}
+
+}
